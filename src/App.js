e2b2e12358b651c7b06b355e7502a1b58c7bf196
NamDo <namdo10121999@gmail.com>
2020-02-20 10:18:31 +0700

    Create directing between SignIn Page and User Page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,86 @@
 import React from 'react';
 import Navigation from './components/Navigation';
 import OrderOnline from './OrderOnline';
-import Menu from './Menu';
-import Contact from './Contact';
-import About from './About';
 import SignIn from './SignIn';
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-          <Navigation />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/order" component={OrderOnline} />
-            <Route path="/menu" component={Menu} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/about" component={About} /> 
-          </Switch>
-      </div>
-    </Router>
-    // <SignIn></SignIn>
-	);
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      route: 'signifn',
+    };
+  }
+  
+  onRouteChange = (route) => {
+    this.setState({
+      route: route
+    })
+  }
+  render(){
+    const {route} = this.state;
+
+    if (route === 'signin') {
+        return (
+          <Router>
+            <div className="App">
+                <Navigation />
+                <Switch>
+                  <Route  path="/" 
+                          exact
+                          render={(props) => <Home {...props} onRouteChange={this.onRouteChange} />} 
+                  />
+                  <Route path="/order" component={OrderOnline} />
+                  <Route path="/menu" component={Menu} />
+                  <Route path="/contact" component={Contact} />
+                  <Route path="/about" component={About} /> 
+                </Switch>
+            </div>
+          </Router>);
+      } 
+      else {
+          return (
+
+            <SignIn onRouteChange={this.onRouteChange}></SignIn>
+          );
+      }
+    } 
 }
 
-const Home = () => {
-    return (
-      <div>
-        <h1> Home page </h1>
-      </div>
-    )
+const Home = (props) => {
+  return (
+    <div>
+      <h1> Home page </h1>
+      <button onClick={()=>props.onRouteChange('signfin')}> Sign out</button>
+    </div>
+  )
 };
+
+const Contact = () => {
+  return (
+    <div>
+      <h1> Contact page </h1>
+    </div>
+  )
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1> About page </h1>
+    </div>
+  )
+};
+
+const Menu = () => {
+  return (
+    <div>
+      <h1> Menu page </h1>
+    </div>
+  )
+};
+
 
 export default App;
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import SignInForm from './components/SignInForm';
 import backgroundImg from './img/sign-in-background.jpg';
+import {Form, Button, Card, Container} from 'react-bootstrap';
 import './SignIn.css';
 
-function SignIn() {
+function SignIn(props) {
     return (
         <div>
             <div className="background">
                 <img src={backgroundImg} alt="background-img" />
             </div>
             <h1 className="logo">Maola</h1>
-            <SignInForm ></SignInForm>
+            <Container style={{height: '100vh'}}>
+            <Card className="pt-4 pb-3" style={{ width: '24rem',top: '160px', left: '650px', borderRadius: '10px'}}>
+                <Card.Title className="text-center"> Sign In </Card.Title>
+                <Card.Body>
+                    <Form onSubmit={()=>{props.onRouteChange('signin')}}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" placeholder="Username" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <div className="text-center">
+                            <Button variant="success" type="submit" style={{top:'10px'}}
+                            >Submit</Button>
+                        </div>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </Container>
       </div>
     )
 }
